f87ef360b6a703d58f86e122052c55893e3740ef
EQIX-TAM-JP <snakayama@ap.equinix.com>
2024-05-29 13:56:15 +0900

    module for NE

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     equinix = {
-      source = "equinix/equinix"
+      source  = "equinix/equinix"
       version = "1.35.0"
     }
   }
@@ -22,4 +22,22 @@ module "instance" {
     nums             = var.nums
     metro            = var.metro
     operating_system = var.operating_system
-}  
+}
+
+module "ne" {
+  source = "./modules/networkedge"
+  # connectivity    = var.connectivity
+  core_count    = var.core_count
+  metro_code    = var.metro
+  notifications = var.notifications
+  package_code  = var.package_code
+  account_number = var.account_number
+  sec_account_number = var.sec_account_number
+  # sec_hostname    = var.sec_hostname
+  sec_metro_code = var.sec_metro_code
+  # sec_name        = var.sec_name
+  # throughput      = var.throughput
+  # throughput_unit = var.throughput_unit
+  type_code = var.type_code
+  ver       = var.ver
+}
